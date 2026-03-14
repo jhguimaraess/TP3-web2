4fcb9cfdb2784467d897c5f02487726189702369
joao guimaraes <guimaraesjoao101@gmail.com>
2026-03-13 21:25:48 -0300

    feat: criar repositories das entities user e role

---
+++ src/main/java/br/edu/ufop/web/carteira/investimentos/carteiraDeinvestimentos/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package br.edu.ufop.web.carteira.investimentos.carteiraDeinvestimentos.repositories;
+
+import br.edu.ufop.web.carteira.investimentos.carteiraDeinvestimentos.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
