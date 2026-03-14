ce551d3f653b16369dca27ed63439ff01490cc2c
joao guimaraes <guimaraesjoao101@gmail.com>
2026-03-13 21:18:21 -0300

    feat: criar model User e Role

---
+++ src/main/java/br/edu/ufop/web/carteira/investimentos/carteiraDeinvestimentos/models/User.java
@@ -0,0 +1,60 @@
+package br.edu.ufop.web.carteira.investimentos.carteiraDeinvestimentos.models;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID userId;
+    private String username;
+    private String email;
+    private String password;
+    private Set<Role> roles;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
